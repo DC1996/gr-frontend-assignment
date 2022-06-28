77485422a61c5b7209a7944f6bae9a18a0615437
Dávid Czirok <d.czirok99@gmail.com>
2022-06-28 17:16:23 +0200

    Created global redux store and added hooks file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import formDataReducer from "../features/formData/formDataSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    form: formDataReducer,
   },
 });
 
